0e8861eeb34f1c30b32c50f8e7ac9ae82ef174f9
tpltnt <tpltnt.github@dropcut.net>
2011-12-24 23:59:53 +0100

    test steps added

--- tests/features/steps.py
+++ tests/features/steps.py
@@ -2,3 +2,15 @@ from lettuce import *
 import sys
 sys.path.append('../../tektronix222/')
 import tek222
+
+@step(u'Given there is no file named')
+def given_there_is_no_file_named(step):
+    assert False, 'This step must be implemented'
+
+@step(u'When the object is initialized')
+def when_the_object_is_initialized(step):
+    assert False, 'This step must be implemented'
+
+@step(u'Then it should open the first file')
+def then_it_should_open_the_first_file(step):
+    assert False, 'This step must be implemented'
