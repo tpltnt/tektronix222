e7d538c3565a055d5a8338d45dcce120fba2b821
tpltnt <tpltnt.github@dropcut.net>
2011-12-25 01:08:39 +0100

    first tests get passed

--- tek222.py
+++ tek222.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python
 
-print "hello you :)"
+class Tek222:
+
+	def __init__(self):
+		__title__ = 'tek222'
+		__version__ = '0.0.1'
+		__author__ = 'tpltnt'
+		__license__ = 'ISC'
+		__copyright__ = 'Copyright 2011 tpltnt'
+		self.usbdevice = '/dev/ttyUSB0'
+
+	def get_serial_port(self):
+		return self.usbdevice
--- tests/features/steps.py
+++ tests/features/steps.py
@@ -1,16 +1,21 @@
 from lettuce import *
 import sys
 sys.path.append('../../tektronix222/')
-import tek222
+from tek222 import Tek222
 
 @step(u'Given there is no file named')
 def given_there_is_no_file_named(step):
-    assert False, 'This step must be implemented'
+	assert True, 'yay!'
 
 @step(u'When the object is initialized')
 def when_the_object_is_initialized(step):
-    assert False, 'This step must be implemented'
+	world.scope = Tek222()
+	assert world.scope, "object initialized %d" % world.scope
 
 @step(u'Then it should open the first file')
 def then_it_should_open_the_first_file(step):
-    assert False, 'This step must be implemented'
+	currentdevice = world.scope.get_serial_port()
+        if '/dev/ttyUSB0' == currentdevice :
+                assert True, 'yay!'
+        else:
+                assert False, 'not first device'
