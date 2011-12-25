3146163d3f3cb467bddc10a64b7f802cca0fa3f7
tpltnt <tpltnt.github@dropcut.net>
2011-12-25 13:04:39 +0100

    new (empty) test steps added

--- tests/features/steps.py
+++ tests/features/steps.py
@@ -15,5 +15,24 @@ def when_the_object_is_initialized(step):
 @step(u'Then it should open the first file')
 def then_it_should_open_the_first_file(step):
 	currentport = world.scope.serialport
-    assert '/dev/ttyUSB0' == currentport, "first port taken"
+	assert '/dev/ttyUSB0' == currentport, "first port taken %s" % currentport
 
+@step(u'Given there is an initialized object')
+def given_there_is_an_initialized_object(step):
+    assert False, 'This step must be implemented'
+
+@step(u'When the oscilloscope is disconnected')
+def when_the_oscilloscope_is_disconnected(step):
+    assert False, 'This step must be implemented'
+
+@step(u'Then the serial port should be closed')
+def then_the_serial_port_should_be_closed(step):
+    assert False, 'This step must be implemented'
+
+@step(u'When the oscilloscope is unplugged')
+def when_the_oscilloscope_is_unplugged(step):
+    assert False, 'This step must be implemented'
+
+@step(u'Then an exception should be raised')
+def then_an_exception_should_be_raised(step):
+    assert False, 'This step must be implemented'
